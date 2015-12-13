f32777a38dad88265b9fbf08d4a8e9fe90eea5ee
Mahmoud <Mahmoud@Mahmoud-PC>
2015-12-13 21:11:43 +0200

    teww

---
+++ src/javaapplication3/NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Mahmoud
+ */
+public class NewClass {
+    
+    public static void main(String[] args) {
+       int x; int y; 
+        x=10;
+        y=3;
+        System.out.println("res:"+(x+y));
+        
+    }
+    
+}
